c9558dcf05f8724388fe636edee2237186e1e12b
Aurelia Perez <aurelia.perez@digitalonus.com>
2021-11-23 10:41:13 -0600

    Add Pullalways

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# az account set --subscription c4774376-bc4c-48e6-93eb-c0ac26c6345d
+# az aks get-credentials --resource-group aurelia-perez --name k8sDevPI
+
+# unset VAR1
+# VAR1=$(helm ls -n boutique | awk 'NR==2{print $1}')
+# if [ "$VAR1" == "boutique" ]; then
+#     helm uninstall boutique -n boutique
+# echo "Entre---------"
+# fi
+# sleep 35
+# helm install boutique -f BoutiqueChart/templates/service.yaml BoutiqueChart  --create-namespace --namespace boutique
+# echo "Pase--------"
+
+helm upgrade boutique -f BoutiqueChart/templates/service.yaml BoutiqueChart --install --create-namespace --namespace boutique
